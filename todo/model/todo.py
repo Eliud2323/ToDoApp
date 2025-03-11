9f8e91ed4fd3175f3483facb58cf43c75b2d4e77
B12_309_21\b12s309 <julioochoamedrano@gmail.com>
2025-03-11 11:44:52 -0500

    commit two

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -1,4 +1,3 @@
-
 class Todo:
     def __init__(self, code_id: int, title: str, description: str):
         self.code_id:int = code_id
@@ -14,4 +13,21 @@ class Todo:
         if tag not in self.tags:
             self.tags.append(tag)
 
+    def __str__(self) -> str:
+            return f"{self.code_id} - {self.title}"
+
+    class TodoBook:
+        def __init__(self):
+            self.todos: dict[int, Todo] = {}
+
+        def add_todo(self, title: str, description: str) -> int:
+            todo_id = len(self.todos) + 1
+            new_todo = Todo(todo_id, title, description)
+            self.todos[todo_id] = new_todo
+            return todo_id
+
+        def pending_todos (self) -> list:
+            return [todo for todo in self.todos.values() if todo.completed]
+
+        def compled_todos (self)
 
